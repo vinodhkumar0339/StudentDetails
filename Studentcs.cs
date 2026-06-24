e651a1056b4348f9e23d190ef238e7d210f7b62c
Vinod Guptha <Vinodhkumar0339@gmail.com>
2026-06-24 12:58:47 +0530

    Initial StudentDetails Project

---
+++ Studentcs.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentDetails
+{
+    public class Studentcs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public string City { get; set; }
+    }
+}
